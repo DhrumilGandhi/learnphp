dc1390ccbe9f301df3e119333d613763511dd373
Dhrumil <dhrumilgandhi540@gmail.com>
2020-10-03 00:22:11 +0530

    change update file using id

--- update1.php
+++ update1.php
@@ -6,7 +6,7 @@ $conn = mysqli_connect('localhost', 'root', '', 'crud');
 
 
 // update query
-    $update = "UPDATE crud_table SET id=$id, uname='$fname',email='$femail' WHERE id = 6";
+    $update = "UPDATE crud_table SET id=$id, uname='$fname',email='$femail' WHERE id = $id";
     $ab = mysqli_query($conn, $update);
     if($ab){
         ?>
